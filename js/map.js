74702b75dc1ff60c506f48f199110d9aabb1c16b
wesleykeller <weskeller19@hotmail.com>
2025-04-11 13:41:21 -0500

    geojson updates

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Load GeoJSON directly from local file
-fetch('deer_data/deer_data.geojson')
+fetch('https://raw.githubusercontent.com/wesleykeller/MN_Deer/36ebbb3da1d50f5cf490c26ee52727f974ba66ca/deer_data/deer_data.geojson')
   .then(response => response.json())
   .then(data => {
     L.geoJSON(data).addTo(map);
